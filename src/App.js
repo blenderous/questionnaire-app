867f936f71814d073044b197be95f2160ad839c1
blenderous <hpone.n.only@gmail.com>
2019-05-24 17:57:06 +0530

    Added functionality to clear button

--- src/App.js
+++ src/App.js
@@ -5,23 +5,55 @@ import { OptionsList } from './OptionsList';
 import questions from './Data';
 
 export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { correctAnswers: 0, wrongAnswers: 0 }
+    this.updateScore = this.updateScore.bind(this);
+  }
+
+  updateScore(answerIndex, number) {
+      console.log('answerIndex ' + answerIndex);
+      console.log('number ' + number);
+      if (answerIndex === number) {
+        this.setState({correctAnswers : this.state.correctAnswers + 1});
+      }
+      else {
+        this.setState({wrongAnswers : this.state.wrongAnswers + 1});
+      }
+  }
+
+  validate(e) {
+    e.preventDefault();
+    console.log('validation comes here');
+  }
+
+  clear(e) {
+    e.preventDefault();
+    var inputElements = document.getElementsByTagName("input");
+    for (let index = 0; index < inputElements.length; index++) {
+      const element = inputElements[index];
+      element.checked = false;
+    }
+  }
   
   render() {
-    const questionsList = questions.map((question, i) =>
+    var questionsList = questions.map((question, i) =>
       <div key={i} className="input-group">
         <p>{i+1}. {question.question}</p>
         <OptionsList
           number={i + 1} 
           answerIndex={question.answerIndex} 
-          options={question.answerOptions}/>
+          options={question.answerOptions}
+          />
       </div>
     );
     return (
       <div className="App">
         <form>
           {questionsList}
-          <button type="submit">Submit</button>
-          <button>Clear</button>
+          <button onClick={this.validate} type="submit">Submit</button>
+          <button onClick={this.clear}>Clear</button>
         </form>
       </div>
     );
--- src/OptionsList.js
+++ src/OptionsList.js
@@ -1,22 +1,7 @@
 import React from 'react';
 
 export class OptionsList extends React.Component {
-
-    constructor(props) {
-      super(props);
-      this.state = { correctAnswers: 0, wrongAnswers: 0 }
-      this.updateScore = this.updateScore.bind(this);
-    }
-
-    updateScore(isCorrect) {
-        if (isCorrect) {
-            this.setState.correctAnswers += 1;
-        }        
-        else {
-            this.setState.wrongAnswers += 1;
-        }
-    }
-
+    
     render() {
         const optionsList = this.props.options.map((option, i) =>
             <div key={i}>
@@ -24,8 +9,12 @@ export class OptionsList extends React.Component {
                     type="radio" 
                     name={'input-' + this.props.number} 
                     id={'input-' + this.props.number + i} 
-                    value={option}/>
-                <label htmlFor={'input-' + this.props.number + i}>
+                    value={option}
+                    />
+                <label
+                    // onClick={this.updateScore(this.props.answerIndex, i)} 
+                    // onClick={this.props.onClick}
+                    htmlFor={'input-' + this.props.number + i}>
                     {option}
                 </label>
                 <br/>
