7f4283a38b0aefc16fcfd60cad2857ed87ea5043
blenderous <hpone.n.only@gmail.com>
2019-05-26 18:57:54 +0530

    trying to open the chart on allAttempted

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import { OptionsList } from './OptionsList';
 import questions from './Data';
+import Chart from 'chart.js';
 
 export class App extends React.Component {
 
@@ -72,13 +73,57 @@ export class App extends React.Component {
     var that = this;
 
     promise.then(function(value){ 
+      console.log(value);
       // value = true
       that.setState({allAttempted: value });
       that.checkAnswers();
-      // 
-      console.log(that.state.correctAnswers);
-      console.log(that.state.wrongAnswers);
+      
+      // console.log(that.state.correctAnswers);
+      // console.log(that.state.wrongAnswers);
+
+      // create bar chart here
+      that.createBarChart();
     })
+  }
+
+  createBarChart() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
   }
 
   checkAnswers() {
@@ -132,6 +177,7 @@ export class App extends React.Component {
           <button onClick={this.validate} type="submit">Submit</button>
           <button onClick={this.clear}>Clear</button>
         </form>
+        <canvas id="myChart" width="400" height="400"></canvas>
       </div>
     );
   }
