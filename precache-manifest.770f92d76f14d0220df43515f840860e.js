74c96122cc27c849f7694acfcb7ea1a490608005
blenderous <hpone.n.only@gmail.com>
2019-05-27 14:55:58 +0530

    Updates

---
+++ precache-manifest.770f92d76f14d0220df43515f840860e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10ff5ba96ef01c26a84c6ef32d264d2e",
+    "url": "/questionnaire-app/index.html"
+  },
+  {
+    "revision": "54a79084fb0c30689b3c",
+    "url": "/questionnaire-app/static/css/main.983aebf1.chunk.css"
+  },
+  {
+    "revision": "3f0b6f21e61103e07d7a",
+    "url": "/questionnaire-app/static/js/2.9ce9aec2.chunk.js"
+  },
+  {
+    "revision": "54a79084fb0c30689b3c",
+    "url": "/questionnaire-app/static/js/main.e93904d7.chunk.js"
+  },
+  {
+    "revision": "6d946ffa71cd4a1655aa",
+    "url": "/questionnaire-app/static/js/runtime~main.bd7ba07e.js"
+  }
+]);
